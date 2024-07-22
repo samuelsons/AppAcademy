0e14430095d0017f55ccf39d297131a797cafb69
Samuel Osoba <samuelsontech@gmail.com>
2024-07-22 18:29:13 +0100

    first commit

---
+++ learnjs.js
@@ -0,0 +1,45 @@
+/* AppAcademy JavaScript Class. */
+
+// let str = "Hello World"
+// let firstInstanceofString = str.indexOf('w')
+
+/* Functions */
+// function average(number1, number2) {
+//     return (number1 + number2) / 2;
+// }
+
+// const result = average(10, 16)
+// console.log(result)
+
+// console.log(average(10, 16))
+
+/* More Functions */
+// function callMe() {
+//     console.log('Second!')
+//     console.log('Third!')
+// }
+
+// console.log('First!')
+// console.log('Fourth!')
+// callMe()
+
+// function sayNumber(number) {
+//     console.log(number)
+// }
+
+// sayNumber(1)
+
+// Functions in JS
+// function averageOfFour(num1, num2, num3, num4) {
+//     //1. Sum the 4 incoming parameters
+//     //2. Divide the sum result by 4
+//     //3. Return the result of the division
+//     let sum = num1 + num2 + num3 + num4
+//     let average = sum / 4
+//     return average
+// }
+
+// console.log(averageOfFour(10, 10, 15, 5))
+
+
+// Conditionals - Control Flow
---
+++ learnpython.py
@@ -0,0 +1,27 @@
+# Variables
+# x = 1   # assign variable x the value 1
+# y = x + 5 # assign variable y the value of x plus 5 > 6
+# z = y # assign z the valye of y which is 6
+
+# Working with boo
+# x = True
+# print(type(x))
+
+# Checking the datatype
+# x = input('Enter your number: ')
+# print(x)
+
+# Accepting inputs.
+# first_number = int(input('Type the first number: ')) ;\
+# second_number = int(input('Type the second number: ')) ;\
+# print("The sum of the first and second number: ", first_number + second_number)
+
+# Maths Operations.
+# difference = 30/12
+# print(difference)
+
+#convertion of time from hours to minutes and minutes to seconds.
+seconds = 1024      #number of minutes in seconds
+display_minutes =  1024 // 60
+print(display_minutes)
+
